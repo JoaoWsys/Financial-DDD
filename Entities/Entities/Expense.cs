9456f7007f67a6531027d27f68c5c5845e2e3830
João Vitor <joaovitorjlle2001@gmail.com>
2023-06-12 21:49:35 -0300

    0001 - Creating Related Entities

---
+++ Entities/Entities/Expense.cs
@@ -0,0 +1,33 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("Expense")]
+    public class Expense : Base
+    {
+        public decimal Value { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public EnumExpenseType ExpenseType { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public bool IsPayed { get; set; }
+        public bool IsLate { get; set;}
+
+        [ForeignKey("Category")]
+        [Column(Order = 1)]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ Entities/Notifications/Notify.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+    public class Notify
+    {
+
+        public Notify()
+        {
+            Notifications = new List<Notify>();
+        }
+
+        [NotMapped]
+        public string PropertyName { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notify> Notifications { get; set; }
+
+        public bool StringPropertyValidate(string value, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(propertyName))
+            {
+                Notifications.Add(new Notify
+                {
+                    Message = "Required Field",
+                    PropertyName = propertyName,
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IntPropertyValidate(int value, string propertyName)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(propertyName))
+            {
+                Notifications.Add(new Notify
+                {
+                    Message = "Required Field",
+                    PropertyName = propertyName,
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
