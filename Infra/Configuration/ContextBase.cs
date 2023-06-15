238b25bb85847b365eb7397986ac13d234831d16
João Vitor <joaovitorjlle2001@gmail.com>
2023-06-14 21:48:30 -0300

    0002 - Adding generic interfaces and repositories.

---
+++ Infra/Configuration/ContextBase.cs
@@ -0,0 +1,44 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options) 
+        {           
+        }
+
+        public DbSet<FinancialSystem> FinancialSystem { get; set; }
+        public DbSet<FinancialSystemUser> FinancialSystemUser { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Expense> Expense { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured) 
+            {
+                optionsBuilder.UseSqlServer(GetConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string GetConnectionString()
+        {
+            return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=False;User ID=sa;Password=1234;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        }
+    }
+}
